fb47ceff9e34c3ccdfcc651c7985d1df8889ecfe
Adel Alhaj <adelalhaj3@gmail.com>
2026-02-12 00:36:27 +0300

    Refactor: light modifications

--- server/seed.py
+++ server/seed.py
@@ -1,12 +1,12 @@
 import asyncio
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from config import settings
 from models import Base, Book, Customer, Order, OrderItem
 from sqlalchemy import text
+from config import DATABASE_URL
 
 # Create database engine
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
+engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def seed_database():
